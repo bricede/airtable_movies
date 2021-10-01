736cbbe6862debf5ab571f47205091723ed4f89c
bricede <bricede@hotmail.com>
2021-10-01 17:23:29 +0200

    show Titles

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const url = `https://api.airtable.com/v0/${keys.db}/movies`;
 const apiKey = keys.apiKey;
 
 let formattedMovies = [];
+const moviesDiv = document.querySelector('.movies'); //récupération de l'élément du DOM (de la classe .movies)
 
 function init() {
     getMovies();
@@ -22,6 +23,14 @@ async function getMovies() {
     console.log('movies', movies);
     formattedMovies = utils.formatData(movies);
     console.log('formattedMovies', formattedMovies);
+    displayMovies(formattedMovies);
+}
+
+function displayMovies(movies) {
+    const cards = movies.map((m) => `<div class="card">${m.Name}</div>`); //récupération d'un tableau de strings
+    // ['<div class="card">Tenet</div>',<div class="card">Danse avec les Loups</div>',...]
+    console.log('cards', cards);
+    moviesDiv.innerHTML = `<h2>Tous les films</h2>${cards.join('')}`;
 }
 
 init();
